8c724c9abbb31fa42cce2314301004454382f105
lethu <lethuzulu345@gmail.com>
2023-07-20 16:30:03 +0200

    first comm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import config from './config.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [persons, setPersons] = React.useState([])
+    useEffect(() => {
+        axios
+            .get(`${config.api_base_url}/person`)
+            .then((response) => {
+                setPersons(response.data)
+            })
+            .catch((error) => console.log(error))
+    }, [])
+
+    const [details, setDetails] = React.useState({ name: '', surname: '' })
+
+    const handleChange = (event) => {
+        setDetails((details) => {
+            return {
+                ...details,
+                [event.target.name]: event.target.value,
+            }
+        })
+    }
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post(
+                `${config.api_base_url}/person/`,
+                details
+            )
+            console.log('response   ', response)
+        } catch (error) {
+            console.log('error  ', error)
+        }
+    }
+
+    return (
+        <div className='App'>
+            <div>
+                <ul>
+                    {persons.length > 0
+                        ? persons.map((person, index) => {
+                              return (
+                                  <li key={index}>
+                                      {person.name} {' - '} {person.surname}
+                                  </li>
+                              )
+                          })
+                        : null}
+                </ul>
+            </div>
+
+            <div style={{ marginLeft: '10px' }}>
+                <input
+                    name='name'
+                    value={details.name}
+                    onChange={handleChange}
+                    placeholder='name'
+                />
+            </div>
+            <div style={{ marginLeft: '10px' }}>
+                <input
+                    name='surname'
+                    value={details.surname}
+                    onChange={handleChange}
+                    placeholder='surname'
+                />
+            </div>
+            <button style={{marginLeft:'10px'}} onClick={handleSubmit}>submit</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
